5fbb2ddd432dccaed16012e6f325072d4bf88f98
Carlos Kieliszewski <ckieliszewski@lernib.com>
2024-11-27 10:22:30 -0500

    added more resilient source tests

--- tests/source.rs
+++ tests/source.rs
@@ -4,123 +4,45 @@ use std::io::Read;
 
 mod common;
 
-#[test]
-fn source_resolver_basics() {
-    let serve_dir = common::serve_dir();
+macro_rules! source_test_list {
+    ($([$name:ident, $path:literal]),+) => {
+        $(
+            ::paste::paste! {
+                #[test]
+                fn [< source_ $name >]() {
+                    let serve_dir = common::serve_dir();
+                    let expected_dir = common::expected_dir();
 
-    let resolver = SourceResolver::new(serve_dir.clone(), "reqaz.local".try_into().unwrap());
+                    let resolver = SourceResolver::new(serve_dir, "reqaz.local".try_into().unwrap());
 
-    let out = resolver
-        .resolve_source(&"/basic.html".try_into().unwrap())
-        .unwrap();
+                    let out = resolver
+                        .resolve_source(&concat!("/", $path).try_into().unwrap())
+                        .unwrap();
 
-    let mut basic_html = File::open(serve_dir.join("basic.html")).unwrap();
+                    let mut expected_file = File::open(expected_dir.join(concat!($path, ".diff"))).unwrap();
 
-    let mut basic_html_content = String::default();
-    basic_html.read_to_string(&mut basic_html_content).unwrap();
+                    let mut html_content = String::default();
+                    expected_file.read_to_string(&mut html_content).unwrap();
 
-    let basic_html_content = common::parse_html_string(&basic_html_content).to_string();
+                    let html_content = common::parse_html_string(&html_content).to_string();
 
-    assert_eq!(
-        basic_html_content,
-        std::str::from_utf8(&out.body).unwrap().to_string()
-    )
+                    assert_eq!(
+                        common::without_newlines(&html_content),
+                        common::without_newlines(std::str::from_utf8(&out.body).unwrap())
+                    )
+                }
+            }
+        )+
+    };
 }
 
-#[test]
-fn source_fetch() {
-    let serve_dir = common::serve_dir();
-    let expected_dir = common::expected_dir();
-
-    let resolver = SourceResolver::new(serve_dir, "reqaz.local".try_into().unwrap());
-
-    let out = resolver
-        .resolve_source(&"/fetch.html".try_into().unwrap())
-        .unwrap();
-
-    let mut expected_file = File::open(expected_dir.join("fetch.html.diff")).unwrap();
-
-    let mut html_content = String::default();
-    expected_file.read_to_string(&mut html_content).unwrap();
-
-    let html_content = common::parse_html_string(&html_content).to_string();
-
-    assert_eq!(
-        common::without_newlines(&html_content),
-        common::without_newlines(std::str::from_utf8(&out.body).unwrap())
-    )
-}
-
-#[test]
-fn source_fetch_css() {
-    let serve_dir = common::serve_dir();
-    let expected_dir = common::expected_dir();
-
-    let resolver = SourceResolver::new(serve_dir, "reqaz.local".try_into().unwrap());
-
-    let out = resolver
-        .resolve_source(&"/fetch_css.html".try_into().unwrap())
-        .unwrap();
-
-    let mut expected_file = File::open(expected_dir.join("fetch_css.html.diff")).unwrap();
-
-    let mut html_content = String::default();
-    expected_file.read_to_string(&mut html_content).unwrap();
-
-    let html_content = common::parse_html_string(&html_content).to_string();
-
-    assert_eq!(
-        common::without_newlines(&html_content),
-        common::without_newlines(std::str::from_utf8(&out.body).unwrap())
-    )
-}
-
-#[test]
-fn source_component_basic_component() {
-    let serve_dir = common::serve_dir();
-    let expected_dir = common::expected_dir();
-
-    let resolver = SourceResolver::new(serve_dir, "reqaz.local".try_into().unwrap());
-
-    let out = resolver
-        .resolve_source(&"/component/basic_component.html".try_into().unwrap())
-        .unwrap();
-
-    let mut expected_file =
-        File::open(expected_dir.join("component/basic_component.html.diff")).unwrap();
-
-    let mut html_content = String::default();
-    expected_file.read_to_string(&mut html_content).unwrap();
-
-    let html_content = common::parse_html_string(&html_content).to_string();
-
-    assert_eq!(
-        common::without_newlines(&html_content),
-        common::without_newlines(std::str::from_utf8(&out.body).unwrap())
-    )
-}
-
-#[test]
-fn source_component_with_other_content() {
-    let serve_dir = common::serve_dir();
-    let expected_dir = common::expected_dir();
-
-    let resolver = SourceResolver::new(serve_dir, "reqaz.local".try_into().unwrap());
-
-    let out = resolver
-        .resolve_source(&"/component/with_other_content.html".try_into().unwrap())
-        .unwrap();
-
-    let mut expected_file =
-        File::open(expected_dir.join("component/with_other_content.html.diff")).unwrap();
-
-    let mut html_content = String::default();
-    expected_file.read_to_string(&mut html_content).unwrap();
-
-    let html_content = common::parse_html_string(&html_content).to_string();
-
-    assert_eq!(
-        common::without_newlines(&html_content),
-        common::without_newlines(std::str::from_utf8(&out.body).unwrap())
-    )
-}
+source_test_list![
+    [basic, "basic.html"],
+    [fetch, "fetch.html"],
+    [fetch_css, "fetch_css.html"],
+    [component_basic, "component/basic_component.html"],
+    [
+        component_with_other_content,
+        "component/with_other_content.html"
+    ]
+];
